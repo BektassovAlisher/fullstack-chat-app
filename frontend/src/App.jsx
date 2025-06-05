b570b17529172b9198a4bbdc310c51df0b052377
BektassovAlisher <aliserbektassov5@gmail.com>
2025-06-05 14:35:32 +0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+import SignUpPage from "./pages/SignupPage";
+import { Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
+
+const App = () => {
+  const { authUser, checkAuth, isChekingAuth, onlineUsers} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser, onlineUsers });
+
+  const {theme} = useThemeStore();
+
+  if (isChekingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
+  return (
+    <div data-theme = {theme}>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,72 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+import Message from "../../../backend/src/models/message.model";
+import { useAuthStore } from "./useAuthStore";
+
+export const useChatStore = create((set, get) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUserLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUserLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Error loading users");
+    } finally {
+      set({ isUserLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Error loading messages");
+      set({ messages: [] });
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Error sending message");
+    }
+  },
+
+  subscribeToNewMessage: () => {
+    const { selectedUser } = get();
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      const isSelectedUser = newMessage.senderId === selectedUser._id;
+      if (isSelectedUser) {
+        set({ messages: [...get().messages, newMessage] });
+      }
+    });
+  },
+
+  unsubscribeFromNewMessage: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
+  },
+
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
